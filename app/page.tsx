fd65424d89f577e4672fa35a9d76f7dba4224ea9
Abhishek291001 <abhishekavsar@gmail.com>
2025-11-21 16:00:31 +0530

    your message

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,47 @@
-import { Button } from "@/components/ui/button";
+
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import SubmitButton from "@/components/ui/SubmitButton";
+
+
 
 export default function Home() {
   return (
-    <Button className="bg-black text-white">Click me</Button>
+    <div className="h-screen flex justify-center items-center">
+    <Card className="max-w-sm min-w-xs  lg:min-w-sm p-4" >
+      <CardTitle className="text-xl font-bold">Login</CardTitle>
+      <CardDescription>
+        Enter your email below to login in your account
+      </CardDescription>
+      <CardContent>
+        <form className="grid gap-8">
+          <div>
+            <Label>Email</Label>
+            <Input
+            className="my-2"
+            placeholder="Example@hello.com"
+            required
+            type="email"
+            name="email"
+            />
+          </div>
+          <SubmitButton title="login" />
+        </form>
+      </CardContent>
+    </Card>
+    </div>
   );
 }
+
+
+
+
+
+  
